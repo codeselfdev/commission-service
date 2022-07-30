8be6f228c5903f649be4a7f3b11b3881c8f8433d
raonak niloy <raonak.niloy@bs-23.net>
2022-07-30 20:47:03 +0600

    code refactored and config updated

--- codes/script.php
+++ codes/script.php
@@ -2,8 +2,20 @@
 
 declare(strict_types=1);
 
+use Commission\Calculation\Application;
+use Commission\Calculation\Exceptions\InputValidationException;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (!isset($argv[1])) {
     die("you are missing to pass first argument which is csv file location.");
 };
+
+try {
+    echo Application::create()
+        ->parse($argv[1]);
+} catch (InputValidationException $e) {
+    die("Please check csv file contents properly, invalid data found.");
+} catch (Exception $e) {
+    die(sprintf('Unexpected error: "%s".', $e->getMessage()));
+}
