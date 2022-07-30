d5b48e6c10dd7a5d4ef9e625fac33bde744f4028
raonak niloy <raonak.niloy@bs-23.net>
2022-07-30 20:44:23 +0600

    pcsv line parsing

--- codes/src/Application.php
+++ codes/src/Application.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace Commission\Calculation;
 
+use Commission\Calculation\DTOs\TransactionDTO;
+use Commission\Calculation\Exception\InputValidationException;
+use Commission\Calculation\Exceptions\InputValidationException as ExceptionsInputValidationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class Application
 {
-    public function __construct() {
+    public function __construct(
+    ) {
     }
 
     /**
@@ -20,11 +24,54 @@ class Application
     public static function create(): Application
     {
         $container = new ContainerBuilder();
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '\config'));
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__, 1). '/config'));
         $loader->load('services.yaml');
 
         $container->compile();
 
         return $container->get(Application::class);
+    }
+
+    /**
+     * @throws InputValidationException
+     */
+    public function getCommissionFees(TransactionDTO $transaction): string
+    {
+        $transaction->validate();
+        // $user = $this->userService->findOrAddNew($userID, $userType);
+        // $precision = $this->currencyService->getPrecision($amount);
+        // $currency = $this->currencyService->findOrAddNew($currency, $precision);
+
+        // $operation = $this->operationService->addNew($date, $type, $amount, $currency, $user);
+        // return $this->currencyService->format($operation->getCurrency(), $operation->getFee());
+
+        return '9';
+    }
+
+    /**
+     * @throws InputValidationException
+     */
+    public function parse(string $csv_path): string
+    {
+        $result = [];
+
+        $content = file_get_contents(dirname(__DIR__, 1). "/".$csv_path);
+        $lines = explode(PHP_EOL, $content);
+
+        foreach ($lines as $line) {
+            $data = str_getcsv($line);
+
+            if (!is_array($data) || (count($data) !== 6)) {
+                throw new ExceptionsInputValidationException();
+            }
+
+            [$date, $userID, $userType, $type, $amount, $currency] = $data;
+
+            $transaction = new TransactionDTO($date, (int)$userID, $userType, $type, (float)$amount, $currency);
+
+            $result[] = $this->getCommissionFees($transaction);
+        }
+
+        return join(PHP_EOL, $result);
     }
 }
