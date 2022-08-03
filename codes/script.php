aea0cfa291ebbd619bd3502e0f05c5c4cac3debf
raonak niloy <raonak.niloy@bs-23.net>
2022-08-03 22:19:00 +0600

    Doc updated and test app condition added

--- codes/script.php
+++ codes/script.php
@@ -7,12 +7,18 @@ use Commission\Calculation\Exceptions\InputValidationException;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Receive csv file location argument
+ */
 if (!isset($argv[1])) {
     die("you are missing to pass first argument which is csv file location.");
-};
+}
 
 try {
-    echo Application::create()
+    /**
+     * print a string from application
+     */
+    echo Application::create(false)
         ->parse($argv[1]);
 } catch (InputValidationException $e) {
     die("Please check csv file contents properly, invalid data found.");
